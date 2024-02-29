a9c7dc9660debfc5e814532f668b59c8cb6c27f1
ragharao <rn436@cam.ac.uk>
2024-02-29 14:41:17 +0000

    updated dijkstra

--- pathFinder.cpp
+++ pathFinder.cpp
@@ -3,9 +3,10 @@
 using namespace std;
  
 // Number of vertices in the graph
-#define numVert 18
+#define numVert 20
 #define srcParent -1
 #define MAX_DIST INT16_MAX
+#define numBlocks 4
 
 enum direction: uint8_t
 {
@@ -21,6 +22,13 @@ typedef struct {
     uint8_t distance;
     direction direction;
 } node;
+
+uint8_t blockIndices[] = {7, 12, 14, 17};
+enum status: uint8_t {
+    NOTCOMPLETED,
+    COMPLETED,
+};
+ status blockStatus[] = {NOTCOMPLETED, NOTCOMPLETED, NOTCOMPLETED, NOTCOMPLETED};
 
 // ------- HELPER FUNCTIONS -------
 // returns vertex (not yet completed) that is the shortest distance from source 
@@ -47,7 +55,29 @@ void printDistance(int distance[numVert]);
 
 // ------- PRIMARY FUNCTIONS -------
 // Implements dijkstra algorithm (to be improved if need be)
-void dijkstra(node graph[numVert][numVert], int source, int destination, int bestPath[numVert], direction bestPathDirections[numVert], int distance[numVert]);
+void dijkstra(node graph[numVert][numVert], int source, int bestPath[numVert], direction bestPathDirections[numVert], int distance[numVert], int paent[numVert]);
+
+uint8_t nextClosestBlock(int distance[numVert], uint8_t blockIndices[], status blockStatus[numBlocks]){
+
+    int minDis = MAX_DIST;
+    int closestBlockIndex = blockIndices[0];
+    for (int i = 0; i < numBlocks; i++) {
+        if (blockStatus[i] == NOTCOMPLETED && distance[blockIndices[i]] < minDis) {
+            minDis = distance[blockIndices[i]];
+            closestBlockIndex = i;
+        }
+    }
+    blockStatus[closestBlockIndex] = COMPLETED;
+    return blockIndices[closestBlockIndex];
+}
+
+void getOptimalPath(int parent[numVert], int destination, int bestPath[numVert], node graph[numVert][numVert], direction bestPathDirections[numVert]) {
+    int index = 0;
+    returnPath(parent, destination, bestPath, &index);
+    returnDirection(graph, bestPath, bestPathDirections);
+}
+
+
 
 // main
 int main()
@@ -61,20 +91,46 @@ int main()
     // contains directions to take in path order
     direction bestPathDirections[numVert - 1];
 
+    // declare parents
+    int parent[numVert];
+
     // distance[i] will hold the shortest distance from source to i
     int distance[numVert]; 
 
     // fill bestPath with the best path
-    dijkstra(graph, 0, 17, bestPath, bestPathDirections, distance);
-    returnDirection(graph, bestPath, bestPathDirections);
+    dijkstra(graph, 0, bestPath, bestPathDirections, distance, parent);
+    printDistance(distance);
+    int next = nextClosestBlock(distance, blockIndices, blockStatus);
+    getOptimalPath(parent, next, bestPath, graph, bestPathDirections);
 
     // for printing best path
+    printPath(bestPath, bestPathDirections);
+
+    dijkstra(graph, next, bestPath, bestPathDirections, distance, parent);
+    getOptimalPath(parent, 5, bestPath, graph, bestPathDirections);
+    printPath(bestPath, bestPathDirections);
+
+    dijkstra(graph, 5, bestPath, bestPathDirections, distance, parent);
+    printDistance(distance);
+    next = nextClosestBlock(distance, blockIndices, blockStatus);
+    getOptimalPath(parent, next, bestPath, graph, bestPathDirections);
+    printPath(bestPath, bestPathDirections);
+
+    dijkstra(graph, next, bestPath, bestPathDirections, distance, parent);
+    getOptimalPath(parent, 6, bestPath, graph, bestPathDirections);
+    printPath(bestPath, bestPathDirections);
+
+    dijkstra(graph, 6, bestPath, bestPathDirections, distance, parent);
+    printDistance(distance);
+    next = nextClosestBlock(distance, blockIndices, blockStatus);
+    getOptimalPath(parent, next, bestPath, graph, bestPathDirections);
     printPath(bestPath, bestPathDirections);
 
     // prints distance from source to every node
     //printDistance(distance);
 
     // more checks
+    /*
     dijkstra(graph, 17, 5, bestPath, bestPathDirections, distance);
     returnDirection(graph, bestPath, bestPathDirections);
     printPath(bestPath, bestPathDirections);
@@ -98,6 +154,9 @@ int main()
     dijkstra(graph, 5, 0, bestPath, bestPathDirections, distance);
     returnDirection(graph, bestPath, bestPathDirections);
     printPath(bestPath, bestPathDirections);
+
+    printDistance(distance);
+    */
 
     return 0;
 }
@@ -140,7 +199,7 @@ int minDistance(int distance[numVert], bool completedSet[numVert])
     return min_index;
 }
 
-void dijkstra(node graph[numVert][numVert], int source, int destination, int bestPath[numVert], direction bestPathDirections[numVert], int distance[numVert]) {
+void dijkstra(node graph[numVert][numVert], int source, int bestPath[numVert], direction bestPathDirections[numVert], int distance[numVert], int parent[numVert]) {
  
     bool completedSet[numVert]; // completedSet[i] will be true if vertex i's distance to 
     // source can no longer be decreased.
@@ -157,7 +216,6 @@ void dijkstra(node graph[numVert][numVert], int source, int destination, int bes
             bestPath[i] = -1;
         }
     // initialise parent set
-    int parent[numVert];
     for (int i = 0; i < numVert; i++) {
         parent[i] = srcParent;
     }
@@ -190,8 +248,6 @@ void dijkstra(node graph[numVert][numVert], int source, int destination, int bes
     // print the constructed distance array
 
     // return best path as an array
-    int index = 0;
-    returnPath(parent, destination, bestPath, &index);
 }
 
 
@@ -202,7 +258,7 @@ void initialise(node graph[numVert][numVert]) {
             graph[i][j].distance = 0;
         }
     }
-        graph[0][3].distance = 46; graph[3][0].distance = 46;
+    graph[0][3].distance = 46; graph[3][0].distance = 46;
     graph[1][2].distance = 71; graph[2][1].distance = 71; 
     graph[1][5].distance = 46; graph[5][1].distance = 46;
     graph[1][8].distance = 85; graph[8][1].distance = 85;
@@ -212,12 +268,14 @@ void initialise(node graph[numVert][numVert]) {
     graph[4][6].distance = 46; graph[6][4].distance = 46;
     graph[4][11].distance = 85; graph[11][4].distance = 85;
     graph[8][9].distance = 102; graph[9][8].distance = 102;
-    graph[8][15].distance = 179; graph[15][8].distance = 179; // bend
+    graph[8][18].distance = 76; graph[18][8].distance = 76;
+    graph[18][15].distance = 103; graph[15][18].distance = 103; // bend
     graph[9][10].distance = 35; graph[10][9].distance = 35;
     graph[9][13].distance = 37; graph[13][9].distance = 37;
     graph[10][11].distance = 72; graph[11][10].distance = 72;
     graph[10][12].distance = 30; graph[12][10].distance = 30;
-    graph[11][16].distance = 139; graph[16][11].distance = 139;// bend
+    graph[19][16].distance = 63; graph[16][19].distance = 63;// bend
+    graph[11][19].distance = 76; graph[19][11].distance = 76;
     graph[13][14].distance = 45; graph[14][13].distance = 45;
     graph[13][15].distance = 39; graph[15][13].distance = 39;
     graph[15][16].distance = 42; graph[16][15].distance = 42;
@@ -233,12 +291,14 @@ void initialise(node graph[numVert][numVert]) {
     graph[4][6].direction = DOWN; graph[6][4].direction = REVERSE;
     graph[4][11].direction = UP; graph[11][4].direction = DOWN;
     graph[8][9].direction = RIGHT; graph[9][8].direction = LEFT;
-    graph[8][15].direction = UP; graph[15][8].direction = DOWN; // bend
+    graph[8][18].direction = UP; graph[18][8].direction = DOWN;
+    graph[18][15].direction = RIGHT; graph[15][18].direction = LEFT; // bend
     graph[9][10].direction = RIGHT; graph[10][9].direction = LEFT;
     graph[9][13].direction = UP; graph[13][9].direction = DOWN;
     graph[10][11].direction = RIGHT; graph[11][10].direction = LEFT;
     graph[10][12].direction = DOWN; graph[12][10].direction = REVERSE;
-    graph[11][16].direction = UP; graph[16][11].direction = DOWN;// bend
+    graph[19][16].direction = LEFT; graph[16][19].direction = RIGHT;// bend
+    graph[11][19].direction = UP; graph[19][11].direction = DOWN;
     graph[13][14].direction = LEFT; graph[14][13].direction = REVERSE;
     graph[13][15].direction = UP; graph[15][13].direction = DOWN;
     graph[15][16].direction = RIGHT; graph[16][15].direction = LEFT;
